131c9e133990392d024d478c2aa9d423053579ff
mgrisafe <mgrisafe@umich.edu>
2014-10-11 14:09:00 -0400

    Did my best to add a few more screens and activities... not the best
    Hi Guys, I added a few more activities and some code to the
    corresponding Java pages (I honestly don't know what I am doing).  I
    couldn't get the input fields to show in the New Login Screen and have
    no idea how to add a drop down menu in the add family screen.  I also
    don't know how to resize the family buttons on the family screen....
    lots of unsolved questions....

---
+++ AndroidFamCamProject/app/src/main/java/com/lilysamimi/famcamproject/AddFamilyMembers.java
@@ -0,0 +1,45 @@
+package com.lilysamimi.famcamproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AddFamilyMembers extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_family_members);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_family_members, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+
+        // When user clicks on finish button go to my familypage
+
+    }
+}
---
+++ AndroidFamCamProject/app/src/main/java/com/lilysamimi/famcamproject/MyFamily.java
@@ -0,0 +1,58 @@
+package com.lilysamimi.famcamproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyFamily extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_family);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_family, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void record(View view {
+        Intent intent= new Intent(this, RecordActivity.class);
+        startActivity(intent);
+    })
+        // When user clicks on record go to record screen
+
+    @Override
+    public void myProfile(View view {
+        Intent intent= new Intent(this, MyProfile.class);
+        startActivity(intent);
+    })
+    // When user clicks on MyProfle go to My Profile screen
+
+
+    @Override
+    public void familyImage(View view {
+        Intent intent= new Intent(this, FamilyImage.class);
+        startActivity(intent);
+    })
+    // When user clicks on any FamilyMember go to Grandma Tata Profile demo screen
+}
