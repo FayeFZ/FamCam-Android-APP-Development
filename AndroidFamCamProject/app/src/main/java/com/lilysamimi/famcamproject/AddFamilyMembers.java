aa4dc39b49bb75543af2847e9cfb25229fd798b1
FayeFZ <lazhaofei@gmail.com>
2014-10-16 11:04:34 -0400

    back

---
+++ AndroidFamCamProject/app/src/main/java/com/lilysamimi/famcamproject/AddFamilyMembers.java
@@ -0,0 +1,65 @@
+// This page was created by Lily Samimi
+
+
+package com.lilysamimi.famcamproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AddFamilyMembers extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_family_members);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_family_members, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+
+        // When user clicks on add from address book we require 3 methods
+        // 1.  pull contacts from address book and phone numbers
+        // 2.  match these numbers to registered members in database
+        // 3.if a match is found, bring in family member and display with add button
+        // 4. if a match is not found display without add button
+
+
+    }
+}
+
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+
+        // When user clicks on finish button go to my familypage
+        // If the user adds a family member, that family member is added on the my family activity screen
+
+    }
+}
