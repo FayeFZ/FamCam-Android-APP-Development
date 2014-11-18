68de64a1f67d212fbd777a9107c8c09c2ebfbe25
mgrisafe <mgrisafe@umich.edu>
2014-11-18 13:58:54 -0500

    Re-added else statement accidently deleted

--- AndroidFamCamProject/app/src/main/java/com/lilysamimi/famcamproject/ListviewMyFamily.java
+++ AndroidFamCamProject/app/src/main/java/com/lilysamimi/famcamproject/ListviewMyFamily.java
@@ -68,7 +68,9 @@ public class ListviewMyFamily extends Activity {
         if (usernameText.equals( "Grandma Tata")) {
             RECORD.setVisibility(View.VISIBLE);
         }
-   
+        else {
+            RECORD.setVisibility(View.GONE);
+        }
         //Mike- Hey Lily, I added the shared preferences that were located in profile here so that it affected if the
         //record button was on or off on the main landing page.  However, this still doesn't seem to be pulling the user entered
         //text from the textview "userNameText"... Right now the "RECORD" is just always gone, so it is understanding
